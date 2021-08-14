ce85fe796827c688d3912b9de4924eb03f90d7dc
leDoanNam <namthamlam505@gmail.com>
2021-08-14 19:41:25 +0700

    remove env

--- config/db/index.js
+++ config/db/index.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-         await mongoose.connect('mongodb+srv://n4msama:nam205806@cluster0.9l5pk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' ,{ useNewUrlParser: true, useUnifiedTopology: true });
+         await mongoose.connect(process.env.DATABASE_URL ,{ useNewUrlParser: true, useUnifiedTopology: true });
          console.log("connected to db !")
 
     } catch (error) {
-        console.log("connected to failure !" , error)
+        console.log("connected to failure !")
     }
 
 }
