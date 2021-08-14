3f4e8ed636d186ea7e31e7bdba5a9911a8045a2d
leDoanNam <namthamlam505@gmail.com>
2021-08-14 19:28:38 +0700

    up

--- config/db/index.js
+++ config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-         await mongoose.connect(process.env.DATABASE_URL ,{ useNewUrlParser: true, useUnifiedTopology: true });
+         await mongoose.connect('mongodb+srv://n4msama:nam205806@cluster0.9l5pk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' ,{ useNewUrlParser: true, useUnifiedTopology: true });
          console.log("connected to db !")
 
     } catch (error) {
