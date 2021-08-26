7f60005c3bcdb2f16514cedca745d40d6ba6c19f
leDoanNam <namthamlam505@gmail.com>
2021-08-26 20:12:21 +0700

    up ahihi cors done

--- app.js
+++ app.js
@@ -1,31 +1,15 @@
 const express = require("express");
 const app = express();
-
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const db = require("./config/db/index");
 require("dotenv").config();
 app.use(bodyParser.json());
-app.options("*", (req, res) => {
-  res.status(200).send("Preflight request allowed");
-});
-const corsOptions ={
-  origin:'http://localhost:3000', 
-  credentials:true,           
-  optionSuccessStatus:200
-  } 
 
-  // app.use(function(req,res,next){
-  //   res.header("Access-Control-Allow-Origin", "*");
-  //   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
-  //   res.header("Access-Control-Allow-Headers", "Accept, Content-Type, Authorization, X-Requested-With");
-  //   next();
-  // });
-
-
+const corsOptions ={credentials: true, origin: true}
 app.use(cors(corsOptions));
 const imagesRoute = require("./routes/images");
-app.use("/api/images" , imagesRoute);
+app.use("/api/images", imagesRoute);
 db.connect();
 let port = process.env.PORT || 5000;
 
