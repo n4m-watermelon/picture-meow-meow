bd307e2bbb4800b8c0ef66b07271c49c47a2c825
leDoanNam <namthamlam505@gmail.com>
2021-08-26 19:04:39 +0700

    up cord

--- app.js
+++ app.js
@@ -11,6 +11,24 @@ app.use(cors());
 // const newsRoute = require("./routes/new")
 const imagesRoute = require("./routes/images");
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 // app.use('/news' , newsRoute)
 app.use("/api/images", imagesRoute);
 db.connect();
