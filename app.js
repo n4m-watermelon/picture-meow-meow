59ba616aa8ef8a469b7283ed7abd0c556688e601
leDoanNam <namthamlam505@gmail.com>
2021-08-26 19:39:09 +0700

    up

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const cors = require("cors");
 const db = require("./config/db/index");
 require("dotenv").config();
 app.use(bodyParser.json());
-
+app.options("*", (req, res) => {
+  res.status(200).send("Preflight request allowed");
+});
 const corsOptions ={
     "origin": "*",
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
@@ -14,14 +16,17 @@ const corsOptions ={
     "optionsSuccessStatus": 204
   } 
 
+  app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
+    res.header("Access-Control-Allow-Headers", "Accept, Content-Type, Authorization, X-Requested-With");
+    next();
+  });
+
+
 app.use(cors(corsOptions));
-
-// const newsRoute = require("./routes/new")
 const imagesRoute = require("./routes/images");
-
-
-// app.use('/news' , newsRoute)
-app.use("/api/images", cors() , imagesRoute);
+app.use("/api/images" , imagesRoute);
 db.connect();
 let port = process.env.PORT || 5000;
 
